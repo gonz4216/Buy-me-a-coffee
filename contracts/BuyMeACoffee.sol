a7976f343cdc48e9dcee917c6d7736a9c671fef5
gonz4216 <ethgon19@gmail.com>
2023-02-05 22:22:44 -0500

    first comit

---
+++ contracts/BuyMeACoffee.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+contract BuyMeACoffee {
+    event NewMemo(
+        address indexed from,
+        uint256 timestamp,
+        string name,
+        string message
+    );
+
+    //Memo strcut
+    struct Memo {
+        address from;
+        uint256 timestamp;
+        string name;
+        string message;
+    }
+    //List of all memos recived form friends.abi
+    Memo[] memos;
+
+    //Address of contract deployer
+    address payable owner;
+
+    //Deploy logic
+    constructor() {
+        owner = payable(msg.sender);
+    }
+
+    function buyCoffee(string memory _name, string memory _message)
+        public
+        payable
+    {
+        require(msg.value > 0, "cant buy coffee with 0 eth");
+
+
+        //emit a log event when a new memo is created!
+        memos.push(Memo(msg.sender, block.timestamp, _name, _message));
+        emit NewMemo(msg.sender, block.timestamp, _name, _message);
+
+
+    }
+
+
+    function withDrawTips() public {
+    require(owner.send(address(this).balance));        
+
+    }
+
+    function getMemos() public view returns(Memo[] memory){
+        return memos;
+    }
+}
